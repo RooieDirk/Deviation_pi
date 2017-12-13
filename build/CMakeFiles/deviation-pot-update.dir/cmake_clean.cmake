dd84a728b4c486f119b4c55c27f3e1967b6dae2d
RooieDirk <dirksmits@zonnet.nl>
2017-12-13 10:45:36 +0100

    remove build files

--- build/CMakeFiles/deviation-pot-update.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/deviation-pot-update"
-  "po/deviation_pi.pot.dummy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/deviation-pot-update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
