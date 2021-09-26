0fc314f28c30be50224184bd75c90419a742d5e3
RooieDirk <r.dirksmits@gmail.com>
2021-09-26 16:07:45 +0200

    Revert "Merge pull request #7 from rgleason/master"
    This reverts commit 3ab774c8f63f4ce78124b757c03f961e8fe0a0f4, reversing
    changes made to a6022c954e6d9bcd10d4a81bcc765a967dd85243.

--- cmake/PluginInstall.cmake
+++ cmake/PluginInstall.cmake
@@ -5,7 +5,7 @@
 ##---------------------------------------------------------------------------
 
 IF(NOT APPLE)
-#  TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${wxWidgets_LIBRARIES} ${EXTRA_LIBS} )
+  TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${wxWidgets_LIBRARIES} ${EXTRA_LIBS} )
 ENDIF(NOT APPLE)
 
 IF(WIN32)
