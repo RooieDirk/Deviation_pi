2fa11f1940d6d316c39b4540a53ea0acd810c0f5
Rick Gleason <fcgleason@tgp-architects.com>
2025-02-04 15:42:37 -0500

    0.1.64.0 + fix macos build

--- ci/circleci-build-macos-universal.sh
+++ ci/circleci-build-macos-universal.sh
@@ -74,7 +74,11 @@ fi
 
 # nor-reproducible error on first invocation, seemingly tarball-conf-stamp
 # is not created as required.
-make package || make package
+# make package || make package
+make
+make install
+make package
+make package
 
 # Create the cached /usr/local archive
 if [ -n "$CI"  ]; then
